6ae1107bfd29f1d5675912a898da73c7ed6fcb92
Pooch63 <99148663+Pooch63@users.noreply.github.com>
2025-03-15 15:04:40 -0400

    Committed route id

---
+++ web/src/lib/Course.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    const { name, id }: { name: string, id: string } = $props();
+</script>
+
+<div class="course">
+    <a href="/{id}" class="name">{name}</a>
+</div>
+
+<style>
+    .course {
+        color: black;
+        width: 100%;
+        padding: 8px 5px;
+    }
+</style>
--- web/src/routes/[id]/unit/[unit]/+page.svelte
+++ web/src/routes/[id]/unit/[unit]/+page.svelte
@@ -4,7 +4,7 @@
     
     import type { PageProps } from './$types';
 	import { type Course } from '$scripts/fetch';
-	let { data }: PageProps & { data: Course } = $props();
+	let { data }: PageProps & { data: { course: Course } } = $props();
 </script>
 
 <Navbar />
@@ -12,6 +12,8 @@
 {#if data == null}
 <h1>Uh oh!</h1>
 <h3>Failed to load quiz data :&x#28;</h3>
+{:else if data.course != null}
+<ModuleLoader data={data.course} />
 {:else}
-<ModuleLoader data={data} />
+<h1>Your connection is corrupted. It's been terminated for safety reasons.</h1>
 {/if}
